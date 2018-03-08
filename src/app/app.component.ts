8a4d2e4bf08610db22787814fbd14afa38daaf85
Michael Hinterdorfer <32036184+MHinterdorfer@users.noreply.github.com>
2018-03-08 15:19:51 +0100

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+interface IPerson {
+  name: string;
+}
+
+interface IToDo {
+  id: number,
+  assignedTo?: string,
+  description: string,
+  done?: boolean
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  public people: Observable<IPerson[]>;
+  public todos: Observable<IToDo[]>;
+
+  constructor(private httpClient: HttpClient) {
+    this.people = httpClient.get<IPerson[]>('http://localhost:8080/api/people');
+    this.todos = httpClient.get<IToDo[]>('http://localhost:8080/api/todos');
+  }
+}
